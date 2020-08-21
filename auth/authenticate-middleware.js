7fec28fa071efe925dbf9dce4f7c3ba55f573e08
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-21 10:06:56 -0600

    added authentication middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,22 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+const secret = require("../constants/secret");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "need to add an authorization token" });
+  }
 };
