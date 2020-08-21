0a5654686b1f23d8965cfa734c20646721d9fcaa
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-21 10:17:33 -0600

    add tests

---
+++ api/server.spec.js
@@ -0,0 +1,14 @@
+// DESIGN /api/auth/register
+// returns status code 201 on successful registration
+
+// Responds with failure if user cannot register
+
+// DESIGN /api/auth/login
+// returns status code 200 on successful login
+
+// returns status code 401 on unsuccessful login
+
+// DESIGN /api/jokes
+// returns status code 401 on unsuccessful login
+
+// returns list of dad jokes on success.
